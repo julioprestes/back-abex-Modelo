628c9be99d1f7782d2bba9f9131a0a1670bacdd6
Micael <micaelxv1@gmail.com>
2024-12-02 12:46:35 -0300

    concluido

--- app.py
+++ app.py
@@ -1,30 +1,26 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 
 # Inicializando a aplicação Flask
 app = Flask(__name__)
+
+# Configurar o Flask-CORS
+CORS(app)
 
 # Carregar o modelo treinado
 model = joblib.load('modelo_randomforest.joblib')
 
 # Função para fazer a previsão
 def predict(Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DPF, Age):
-    # Organizar os dados em um formato adequado para o modelo (uma lista ou array)
     features = [[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DPF, Age]]
-    
-    # Fazer a previsão
     prediction = model.predict(features)
-    
-    # Retornar o resultado
     return {"prediction": int(prediction[0])}
 
 @app.route('/predict', methods=['POST'])
 def predictions():
     if request.method == 'POST':
-        # Receber os dados JSON da requisição
         data = request.get_json()
-        
-        # Obter os valores das variáveis
         Age = data.get('Age')
         Pregnancies = data.get('Pregnancies')
         Glucose = data.get('Glucose')
@@ -33,15 +29,9 @@ def predictions():
         BMI = data.get('BMI')
         SkinThickness = data.get('SkinThickness')
         DPF = data.get('DPF')
-        
-        # Chamar a função de previsão com os dados recebidos
         result = predict(Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DPF, Age)
-        
-        # Retornar o resultado como JSON
         return jsonify(result)
-    
     return "Invalid request method"
 
 if __name__ == '__main__':
-    # Rodar o servidor Flask na porta 8000
     app.run(host='0.0.0.0', port=8000, debug=True)
